1af08bd09cfaa5f406500a7aef715f50653da520
Mathew Grabau <mgrabau@mathewgrabau.ca>
2017-11-07 14:56:04 -0600

    Adding a customer listing and details (including the links for it).

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private List<Customer> _customers = new List<Customer>
+        {
+            new Customer {Name = "John Smith", Id = 1},
+            new Customer {Name = "Mary Williams", Id = 2}
+        };
+        
+        // GET
+        public ActionResult Index()
+        {
+            return View(_customers);
+        }
+
+        /// <summary>
+        /// Action to show the customer listing.
+        /// </summary>
+        /// <param name="id">The identifier of the customer to show.</param>
+        /// <returns></returns>
+        public ActionResult Details(int? id)
+        {
+            if (!id.HasValue || id > _customers.Count)
+            {
+                // return a standard 404 for right now.
+                return HttpNotFound();
+            }
+
+            return View(_customers[id.Value - 1]);
+        }
+    }
+}
