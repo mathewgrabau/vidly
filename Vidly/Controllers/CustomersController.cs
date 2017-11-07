4e67f481d4deb84bfc7b3b1dc2bca8b29d41db0e
Mathew Grabau <mgrabau@mathewgrabau.ca>
2017-11-07 16:25:07 -0600

    Loading and displaying the membership type from the database during the customers query.

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web.Mvc;
 using Vidly.Models;
@@ -24,7 +25,7 @@ namespace Vidly.Controllers
         // GET
         public ActionResult Index()
         {
-            var customers = _context.Customers.ToList();
+            var customers = _context.Customers.Include(c=>c.MembershipType).ToList();
 
             return View(customers);
         }
