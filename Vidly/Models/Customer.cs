ec15e13c91f64e6965e7dc1ae85e83025fb7143e
Mathew Grabau <mgrabau@mathewgrabau.ca>
2017-11-07 23:36:53 -0600

    Updated label for the forms.

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
@@ -14,6 +15,7 @@ namespace Vidly.Models
         public string Name { get; set; }
         public bool IsSubscribedToNewsletter { get; set; }
 
+        [DisplayName("Date of Birth")]
         public DateTime? BirthDate { get; set; }
 
         // Navigation property (can add the reference)
