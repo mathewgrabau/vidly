1fbd038cec37b92585e94c0a8f9790eb197696fb
Mathew Grabau <mgrabau@mathewgrabau.ca>
2017-11-08 17:56:03 -0600

    In progress, working on validation.

---
+++ Vidly/Models/Min18YearsOldToBeMember.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class Min18YearsOldToBeMember : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            return base.IsValid(value, validationContext);
+        }
+    }
+}
