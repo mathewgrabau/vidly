8d6d85e8108650c97cc1e48f1da418a7d466f4bf
Mathew Grabau <mgrabau@mathewgrabau.ca>
2017-11-15 12:15:19 -0600

    Working on adding rentals support. In progress code

---
+++ Vidly/Dtos/NewRentalsDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Dtos
+{
+    public class NewRentalsDto
+    {
+        /// <summary>
+        /// Customer that is doing the rental.
+        /// </summary>
+        public int CustomerId { get; set; }
+
+        /// <summary>
+        /// The list of the movies that are to be rented.
+        /// </summary>
+        public List<int> MovieIds { get; set; }
+    }
+}
